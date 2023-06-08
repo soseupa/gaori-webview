91031b86ede4379586afddf3ae4e973d9e64bd89
kimhyerin0909 <khyerin0909@gmail.com>
2023-06-08 16:06:11 +0900

    Add - home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/global.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Map } from "./pages/map";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Home } from "./pages/home";
 
 const App = () => {
   const client = new QueryClient();
@@ -10,6 +11,7 @@ const App = () => {
     <QueryClientProvider client={client}>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/map" element={<Map />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Home = () => {
+  return <div>가오리 웹뷰입니다!</div>;
+};
