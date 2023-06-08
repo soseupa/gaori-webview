2612e26d692ef9b6c269194174194fe6f37e306c
kimhyerin0909 <khyerin0909@gmail.com>
2023-06-09 00:12:37 +0900

    Chore - 필요없는 코드 삭제

--- src/apis/schedule/schedule.api.ts
+++ src/apis/schedule/schedule.api.ts
@@ -1,5 +1,4 @@
 import { server } from "../server";
-import { SCHEDULE_LIST } from "./schedult.type";
 
 export const getSchedule = async ({ id, token }: { id: number; token: string }) => {
   return (
