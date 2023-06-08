5def29b4f64527ba987d2201ccc60aae2a518406
kimhyerin0909 <khyerin0909@gmail.com>
2023-06-08 14:07:47 +0900

    Fix - webpack config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,9 +4,6 @@ const common = require("./webpack.common");
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
-  resolve: {
-    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
-  },
   devServer: {
     open: false,
     hot: true,
--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const { kakao } = window;
+
 export const Map = () => {
   return <div>Map</div>;
 };
